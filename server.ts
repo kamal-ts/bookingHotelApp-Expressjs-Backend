ad3f178d194ffbe4ee7b572743b482960d537bed
kamal1234567890 <kamaluddin1623@gmail.com>
2023-02-06 21:19:55 +0900

    modify port

--- server.ts
+++ server.ts
@@ -1,9 +1,9 @@
-import { config, Application } from "./deps.ts";
+import { Application } from "./deps.ts";
 
 import UserRoute from "./src/routes/Users.ts";
 
-const envVars = await config();
-const port = 5000 || envVars.PORT ;
+// const envVars = await config();
+const port = 5000;
 const app = new Application();
 
 // Logger
