93098c670eb11f1469d47fe0fda332c792b93c49
kamal1234567890 <kamaluddin1623@gmail.com>
2023-02-06 20:37:00 +0900

    url modify

--- src/utils/Databases.ts
+++ src/utils/Databases.ts
@@ -7,7 +7,7 @@ const envVars = await config();
 const db = new PrismaClient({
     datasources: {
       db: {
-        url: "prisma://aws-us-east-1.prisma-data.com/?api_key=sHQsq89N_O1Muy0J3eZwpeXPwgmHS2XrPhj0ReMqprQGy-xsA9GAJS6Zpuwyo33R"
+        url: envVars.DATABASE_URL
       },
     },
   });
