9a0d632e070f3ee762f3c4918b7ea0a4a798c9b8
kamal1234567890 <kamaluddin1623@gmail.com>
2023-04-04 02:59:25 +0900

    test 1

--- server.mjs
+++ server.mjs
@@ -1,29 +1,29 @@
 import express from 'express'
-import UserRoute from './src/routes/Users.mjs'
-import HotelRoute from './src/routes/Hotels.mjs'
+// import UserRoute from './src/routes/Users.mjs'
+// import HotelRoute from './src/routes/Hotels.mjs'
 
 const app = express();
 
 app.use(express.json());
-app.use('/v1/users', UserRoute);
-app.use('/v1/hotels', HotelRoute);
+// app.use('/v1/users', UserRoute);
+// app.use('/v1/hotels', HotelRoute);
 
 
 app.get('/', (_req, res) => {
     res.send('Hello World!');
 });
 
-app.use((err, _req, res, next) => {
-    const errorStatus = err.status || 500;
-    const errorMessage = err.message || "Something went wrong!";
+// app.use((err, _req, res, next) => {
+//     const errorStatus = err.status || 500;
+//     const errorMessage = err.message || "Something went wrong!";
 
-    return res.status(errorStatus).json({
-        success: false,
-        status: errorStatus,
-        message: errorMessage,
-        stack: err.stack,
-    });
-})
+//     return res.status(errorStatus).json({
+//         success: false,
+//         status: errorStatus,
+//         message: errorMessage,
+//         stack: err.stack,
+//     });
+// })
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
